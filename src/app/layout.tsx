81b82425e4f4db5bf66f7b78aa094ddebd5e2e74
Saman Tohidian <31966330+samooel@users.noreply.github.com>
2026-07-17 18:41:01 +0330

    fix-icon-touch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,12 +43,12 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: '/favicon-48.png', sizes: '48x48', type: 'image/png' },
+      { url: '/icon.png', sizes: '48x48', type: 'image/png' },
       { url: '/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
       { url: '/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
     ],
-    shortcut: '/favicon-48.png',
-    apple: '/apple-touch-icon.png',
+    shortcut: '/icon.png',
+    apple: '/apple-icon.png',
   },
   appleWebApp: {
     capable: true,
